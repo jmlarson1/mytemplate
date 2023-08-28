86401c57717656396b176878a31336cd56233e65
Jeffrey Larson <jmlarson@anl.gov>
2023-08-28 14:58:18 -0500

    Let's try to decrease coverage

--- subA/py/compute_a.py
+++ subA/py/compute_a.py
@@ -5,7 +5,8 @@ def compute_a(x):
     filename = Path(__file__).resolve()
     print()
     print(f"{filename} called")
-    if False:
+    
+    if x < -100:
         print("this line should never be covered")
         k = 1+1
         print("this line also should never be covered")
