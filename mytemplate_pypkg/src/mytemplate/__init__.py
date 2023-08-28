ad65f7747fd24acfa80b645be5018ba05e653488
Jeffrey Larson <jmlarson@anl.gov>
2023-08-28 15:21:28 -0500

    Increasing coverage

--- mytemplate_pypkg/src/mytemplate/__init__.py
+++ mytemplate_pypkg/src/mytemplate/__init__.py
@@ -1,10 +1,7 @@
 """
 """
 
-try:
-    from importlib.metadata import version
-except ModuleNotFoundError:
-    from importlib_metadata import version
+from importlib.metadata import version
 
 __version__ = version("mytemplate")
 
