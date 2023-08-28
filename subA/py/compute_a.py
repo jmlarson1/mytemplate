d54968e59b35567c750118aa13aa8e75ed59ae4e
Jeffrey Larson <jmlarson@anl.gov>
2023-08-28 15:19:37 -0500

    Revert

--- subA/py/compute_a.py
+++ subA/py/compute_a.py
@@ -5,9 +5,4 @@ def compute_a(x):
     filename = Path(__file__).resolve()
     print()
     print(f"{filename} called")
-    
-    if x < -100:
-        print("this line should never be covered")
-        k = 1+1
-        print("this line also should never be covered")
     return x**2
